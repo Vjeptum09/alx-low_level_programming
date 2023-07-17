83fc95bb9b7e0c81819e669b588c9114bba5c2b1
Vjeptum09 <vivianjeptum001@gmail.com>
2023-07-17 06:01:42 -0700

    printing code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - Serves as the starting point for program execution
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*prints a string*/
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
