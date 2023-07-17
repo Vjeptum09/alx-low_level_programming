158b49b61c4423b0491edaa92ee70cf63e12859c
Vjeptum09 <vivianjeptum001@gmail.com>
2023-07-17 05:11:38 -0700

    first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * D - A program that prints a line woth put function
  *
- * Description - A program that prints a line with puts function
- *
- * Return: Always 0 (Success)
+ * Return:Always  0 (Success)
  */
 int main(void)
-{	/*print a string*/
+{ printf 
+	/*print a string*/
 	printf("Programming is like building a multilingual puzzle")
-
     return (0);
-}
+} 
+gcc 4-puts.c -o 4-puts.c.exe
