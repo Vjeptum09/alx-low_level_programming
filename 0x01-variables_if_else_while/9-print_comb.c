a0c42f83f971220e97fe3944b89596088319a7c6
Vjeptum09 <vivianjeptum001@gmail.com>
2023-07-22 04:49:09 -0700

    numbers with spaces and commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print numers to 9 with a comma'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 0;
+
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		if (n! = '9');
+		putchar(n);
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	n++;
+	}
+	putchar('\n');
+	return (0);
+}
