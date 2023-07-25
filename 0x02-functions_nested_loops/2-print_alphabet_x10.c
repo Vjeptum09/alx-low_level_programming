ed8b30310673be6bd7d7670e8a3c6c2da9b48218
Vjeptum09 <vivianjeptum001@gmail.com>
2023-07-25 02:36:44 -0700

    printing alphabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,12 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (j=1; i <= 10; i++)
+	for (j = 1; i <= 10; i++)
 	{
-		for (j =97; j<=122; j++)
+		for (j = 97; j <= 122; j++)
 		{
-			putchar(j);
+			_putchar(j);
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
-]
+}
