078449489826273889971e686b611ae93d8bb397
Vjeptum09 <vivianjeptum001@gmail.com>
2023-07-25 02:29:20 -0700

    printing alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
  * main - Entry point
- * Description: 'printing small letter alphabets'
- * return:always 0
+ * print_alphabet - printing small letter alphabets
+ * Return: 0 (Success)
  */
 void print_alphabet(void)
 {
 	int i;
-	for (i=97; i<= 122; i++)
+
+	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
