1e47f147f989c27b56acbb0733c89d670ee27c39
Vjeptum09 <vivianjeptum001@gmail.com>
2023-07-25 10:43:04 -0700

    checking table

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,33 +1,24 @@
 #include "main.h"
-
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_sign -entry point
+ * @n: carrier variable
+ * Description: print the sign of a number
+ * Return : 1 if n>0 0 if n == 0, -1 if n<0
  */
-int main(void)
+int print_sign(int n)
 {
-	int r;
+	if (n > 0)
 
-	r = print_sign(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('0');
+
 	return (0);
 }
