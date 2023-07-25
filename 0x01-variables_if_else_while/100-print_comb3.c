5d0685850cb10e5730c18570d48ba68e7f9c8059
Vjeptum09 <vivianjeptum001@gmail.com>
2023-07-24 23:46:23 -0700

    numbers with spaces

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Descrption: 'numbers in smallest combination'
+ * Return: always 0
+ */
+int main(void)
+{
+	int i = '0';
+	int j = '0';
+
+	while (i <= '9')
+
+
+	{
+		while (j <= '9')
+
+		{
+			if (!(i > j) || i == j)
+			{
+				putchar(i);
+				putchar(j);
+			{
+
+				if (i == '8' && j == '9')
+					continue;
+			}
+
+			}
+			{
+				putchar('\n');
+			}
+			{
+				putchar(' ');
+			}	putchar(',');
+		}
+		j++;
+	}
+	j = '0';
+	i++;
+	return ('0');
+}
