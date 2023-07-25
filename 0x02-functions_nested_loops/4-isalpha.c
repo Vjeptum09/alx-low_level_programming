31ee70ad3d51cd1c1182cd123dc0c0bcea00891f
Vjeptum09 <vivianjeptum001@gmail.com>
2023-07-25 10:15:12 -0700

    playing with alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - function checks alphabets either lower or upper cases
+ *
+ * @c -int used in arguments
+ *
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
