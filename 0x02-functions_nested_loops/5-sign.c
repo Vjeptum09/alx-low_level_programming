b1e6ed90c6cdb087d97f57529b87bf22c0f00b04
Vjeptum09 <vivianjeptum001@gmail.com>
2023-07-26 00:35:50 -0700

    printing signs of numbers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_sign -entry point
+ * print_sign - entry point
  * @n: carrier variable
  * Description: print the sign of a number
- * Return : 1 if n>0 0 if n == 0, -1 if n<0
+ * Return: 1 if n > 0 0 if n == 0, -1 if n < 0
  */
 int print_sign(int n)
 {
@@ -18,7 +18,9 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
-	_putchar('0');
-
-	return (0);
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 }
