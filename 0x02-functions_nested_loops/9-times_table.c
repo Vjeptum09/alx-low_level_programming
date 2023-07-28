fa6df3e4b54cf51a9407b2179de8f8720f711834
Vjeptum09 <vivianjeptum001@gmail.com>
2023-07-28 12:02:48 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,8 +21,8 @@ void times_table(void)
 			_putchar(n + '0');
 			if (j == 9)
 				continue;
-			_putchar(' ');
 			_putchar(',');
+			_putchar(' ');
 
 			}
 			else
@@ -32,8 +32,8 @@ void times_table(void)
 				_putchar((n % 10) + '0');
 				if (j == 9)
 					continue;
-				_putchar(' ');
 				_putchar(',');
+				_putchar(' ');
 			}
 		}
 	_putchar('\n');
