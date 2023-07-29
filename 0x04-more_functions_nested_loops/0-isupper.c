f260cb1518b23ed07543f84223cfc4434f003a5c
Vjeptum09 <vivianjeptum001@gmail.com>
2023-07-29 01:43:02 -0700

    _isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * isupper - checks is a character is uppercase
+ * _isupper - checks is a character is uppercase
  * @c: carrier integer variable
  * Return: 1 if true, 0 if false
  */
-int _isupper(int x)
+int _isupper(int c)
 {
 
-	if (x >= 65 && x <= 90)
+	if (c >= 65 && c <= 90)
 
 	{
 
