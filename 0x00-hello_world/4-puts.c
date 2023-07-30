8dbe083fa0a23f25c8722081231295a9bd520a59
Vjeptum09 <vivianjeptum001@gmail.com>
2023-07-30 01:14:06 -0700

    puts fucntion

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
-
-/**
- * D - A program that prints a line woth put function
- *
- * Return:Always  0 (Success)
- */
 int main(void)
-{ printf 
-	/*print a string*/
-	printf("Programming is like building a multilingual puzzle")
-    return (0);
-} 
-gcc 4-puts.c -o 4-puts.c.exe
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
