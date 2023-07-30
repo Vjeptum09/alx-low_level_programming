8d6f22865039e7dc3329a4c37fd527768a08cda7
Vjeptum09 <vivianjeptum001@gmail.com>
2023-07-30 01:18:45 -0700

    print puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description: ' prints puts'
+ * Return: 0
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
